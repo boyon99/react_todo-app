35e3a1435837cc894c8407e8e222c9cbd53d552a
boyon99 <bo_yon_99@naver.com>
2023-03-20 21:16:04 +0900

    :hammer: Add layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
-    <TodoTemplate>Todo</TodoTemplate>
+    <TodoTemplate>
+      {/* children의 값으로  <TodoInsert />와 <TodoList />가 전달된다. */}
+      <TodoInsert />
+      <TodoList />
+    </TodoTemplate>
   );
 }
 
--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -5,6 +5,7 @@ const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
       <div className="app-title">일정 관리</div>
+      {/* children의 값으로  <TodoInsert />와 <TodoList />가 전달되고 있다. */}
       <div className="content">{children}</div>
     </div>
   );
