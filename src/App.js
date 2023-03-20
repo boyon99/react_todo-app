2e046283fec5dbeec2672615ba596410eff536d1
boyon99 <bo_yon_99@naver.com>
2023-03-20 21:39:49 +0900

    feat: Add app에서 todos 상태 사용하기

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
+import { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([{
+    id: 1,
+    text: '리액트의 기초 알아보기',
+    checked: true
+  },
+  {
+    id: 2,
+    text: '컴포넌트 스타일링 해보기',
+    checked: true
+  },
+  {
+    id: 3,
+    text: '일정관리',
+    checked: false
+  }])
   return (
     <TodoTemplate>
       {/* children의 값으로  <TodoInsert />와 <TodoList />가 전달된다. */}
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 }
