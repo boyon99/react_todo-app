ebb530396a86f7efd92021d08a84969114b9ad77
boyon99 <bo_yon_99@naver.com>
2023-03-21 12:49:55 +0900

    :hammer: feat: Add 일정 추가하기 기능 구현

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
-import { useState } from 'react';
+import { useState, useRef, useCallback } from 'react';
 
 function App() {
   const [todos, setTodos] = useState([{
@@ -20,10 +20,25 @@ function App() {
     text: '일정관리',
     checked: false
   }])
+
+  // id값은 ref를 사용하여 변수로 담기
+  const nextId = useRef(4)
+
+  // todolist를 목록으로 집어넣는 함수
+  const onInsert = useCallback(text => {
+    const todo = {
+      id: nextId.current, text, checked: false
+    }
+    setTodos(todos.concat(todo));
+    nextId.current += 1
+  },
+    [todos]
+  )
+
   return (
     <TodoTemplate>
       {/* children의 값으로  <TodoInsert />와 <TodoList />가 전달된다. */}
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos} />
     </TodoTemplate>
   );
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -4,10 +4,28 @@ import './TodoInsert.scss';
 
 const TodoInsert = ({ onInsert }) => {
 
+  const [value, setValue] = useState('')
+  // 한 번 함수를 만들고 재사용할 수 있도록 구현
+  const onChange = useCallback(e => {
+    console.log("onChange")
+    setValue(e.target.value)
+  }, [])
+
+  // 전송하기
+  const onSubmit = useCallback(
+    e => {
+      onInsert(value);
+      setValue("")
+      // submit 이벤트는 브라우저에서 새로고침을 발생시키는데 이를 방지하기 위해 함수 호출
+      e.preventDefault()
+    }, [onInsert, value]
+  )
   return (
-    <form className="TodoInsert">
+    <form className="TodoInsert" onSubmit={onSubmit}>
       <input
         placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
       />
       <button type="submit">
         <MdAdd />
